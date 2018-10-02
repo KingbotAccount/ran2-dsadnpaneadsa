eff20adcaa8a2870db5d2d220ccbf17522c231ad
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-10-02 19:26:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,19 @@ const prefix = "="
 console.log(`hello`)
 
 
+   client.on('message', message => {
+	   if(message.content.startsWith(`${prefix}invite`)){
+		   if(!message.channel.guild) return message.channel.send("This Command is Just For Servers!")
+		   var embed = new Discord.RichEmbed()
+		   .setTitle(">> ClickHere To Add" + `${client.user.username}` + " <<")
+		   .setURL("https://discordapp.com/oauth2/authorize?client_id=" + `${client.user.id}` + "&scope=bot&permissions=2080374975")
+		   .setTimestamp()
+		   .setFooter(`Requested By | ${message.author.username}`)
+		   .setColor("RANDOM")
+		   message.channel.send(":white_check_mark: | Check Your DM! تم الأرسال بلخاص")
+		   message.author.send({embed})
+	   }
+   });
 
 
 
